533cd780d31585617eeeae67b51835e6a6ba8392
Mujeeb <mujeeb.onlinecourses@gmail.com>
2019-09-07 08:22:04 +0500

    Add Logout functionality from frontend.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
-#from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
@@ -11,5 +9,4 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('unpublished/', views.unpublished_posts, name='unpublished_posts'),
     path('post/<int:pk>/publish/', views.post_publish, name="post_publish"),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
